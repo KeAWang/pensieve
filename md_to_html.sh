c2d9916dd9da0e4fda1a3902160863ac54788e24
Alex Wang <kaw293@cornell.edu>
2022-01-31 17:37:41 -0800

    add makefile for converting md to html

--- md_to_html.sh
+++ md_to_html.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-filepath="$1"
-
-filename="$(basename -- "$filepath")"
-extension="${filename##*.}"
-filename="${filename%.*}"
-
-parent_dir="$(dirname "$filepath")"
-
-pandoc $1 \
-    -H <(echo '\['; cat ./alex-macros.tex; echo '\]') \
-    --mathjax \
-    --standalone \
-    -o ""$parent_dir"/"$filename".html" \
-    -f markdown+tex_math_single_backslash
+for filepath in "$@"
+do
+    filename=$(basename -- "$filepath")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    
+    parent_dir="$(dirname "$filepath")"
+    
+    pandoc $filepath \
+        -H <(echo '\['; cat ./alex-macros.tex; echo '\]') \
+        --mathjax \
+        --standalone \
+        -o "$parent_dir"/"$filename".html \
+        -f markdown+tex_math_single_backslash
+done
